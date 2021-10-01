d5b1c41af99b268df391b9e926021026518e9f4b
Andrew <chandrean@yandex.ru>
2021-10-02 01:09:41 +0300

    добавил типы и шаблон данных

---
+++ src/constants.ts
@@ -0,0 +1,49 @@
+import { v4 as uuid } from "uuid";
+import { IResume } from "./types";
+
+export const defaultRes: IResume = {
+  id: uuid(),
+  name: "",
+  lastName: "",
+  patronymic: "",
+  photo: "",
+  city: "",
+  birthDate: new Date(),
+  gender: "male",
+  citizen: "",
+  position: "",
+  salary: 0,
+  currency: "rub",
+  about: "",
+  experience: true,
+  workplace: [
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      toPresent: true,
+      companyName: "",
+      position: "",
+      cases: "",
+    },
+  ],
+  education: [
+    {
+      level: "",
+      language: "",
+      otherLanguage: [
+        {
+          language: "",
+          level: "",
+        },
+      ],
+      placeOfStudy: [
+        {
+          name: "",
+          faculty: "",
+          speciality: "",
+          endYear: 2000,
+        },
+      ],
+    },
+  ],
+};
---
+++ src/types.ts
@@ -0,0 +1,47 @@
+export type IResume = {
+    id: string;
+    name: string;
+    lastName: string;
+    patronymic?: string;
+    photo?: string;
+    city: string;
+    birthDate: Date;
+    gender: string;
+    citizen: string;
+    position: string;
+    salary: number;
+    currency: string;
+    about?: string;
+    experience: boolean;
+    workplace?: IWorkplace[];
+    education: IEducation[];
+
+}
+
+type IWorkplace ={
+    startDate: Date;
+    endDate?: Date;
+    toPresent?: boolean;
+    companyName: string;
+    position: string;
+    cases?: string;
+}
+
+type IEducation ={
+    level: string;
+    language: string;
+    otherLanguage?: ILanguage[];
+    placeOfStudy: ISchool[];
+}
+
+type ILanguage = {
+    language: string;
+    level: string;
+}
+
+type ISchool = {
+    name: string;
+    faculty: string;
+    speciality: string;
+    endYear: number;
+}
